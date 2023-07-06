3d813044506f5fa1341d6e62850618106ce1413e
Colton Horn <colton.horn@hotmail.com>
2023-07-05 23:59:00 -0400

    elevator pitch

--- src/App.vue
+++ src/App.vue
@@ -106,7 +106,7 @@ export default {
         {
           "slug": "003",
           "name": "Cybersleuth",
-          "status": "start"
+          "status": "success"
         },
       ],
       "pilots": [
