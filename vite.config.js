1dcc905f86138b32cf6d76969d4602e3314b811d
DirkMcThermot <colton.horn@hotmail.com>
2025-04-03 15:16:43 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-const __dirname = import.meta.dirname;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "./",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(import.meta.dirname, './src'),
     },
   }
 })
